0fe3adbca7d31e44f91b792a84eeeef95ba5319d
Mike Yiu <mikesun@mikes-macbook-air.local>
2018-01-21 14:29:13 -0500

    Initial Commit

---
+++ flix_demo_03/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  flix_demo_03
+//
+//  Created by Mike Sun on 1/21/18.
+//  Copyright © 2018 Mike Yiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
